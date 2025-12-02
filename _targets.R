32c1c282e089b978a083ecc905d73680f21bdf40
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-12-02 06:51:47 -0800

    add targets

--- _targets.R
+++ _targets.R
@@ -1,11 +1,17 @@
 library(targets)
 
-# Useful targets commands
+# Create targets for all objects
 # targets::tar_make(script = "_targets.R")
+# Load existing targets
+# targets::tar_load_everything()
+
+# View network plots
 # targets::tar_visnetwork(targets_only = TRUE)
 # targets::tar_glimpse()
+
+# Use the following commands to remove one or all files when getting errors
 # targets::tar_delete("rank")
-# targets::tar_load_everything()
+# targets::tar_destroy("all)
 
 # Set target-specific options such as packages:
 targets::tar_option_set(
@@ -13,15 +19,27 @@ targets::tar_option_set(
     "dplyr",
     "nwfscSurvey",
     "r4ss",
-    "readr",
-    "westcoastdata"
+    "readr" #,
+    #"westcoastdata"
   )
 )
 
+# Load in Rdata object for the WCGBT bio data
+load("data-raw/bio_pull_all_NWFSC.Combo_2025-09-12.rdata")
+
 # Source all functions in the R folder
 targets::tar_source()
-targets::tar_source(
+source(
   "C:/Users/chantel.wetzel/Documents/github/prioritization/data_summary/R/get_species_list.R"
+)
+source(
+  "C:/Users/chantel.wetzel/Documents/github/prioritization/data_summary/R/clean_nwfsc_hkl.R"
+)
+source(
+  "C:/Users/chantel.wetzel/Documents/github/prioritization/data_summary/R/clean_wcgbt_bio.R"
+)
+source(
+  "C:/Users/chantel.wetzel/Documents/github/prioritization/data_summary/R/summarize_new_survey_information.R"
 )
 
 # End this file with a list of target objects.
@@ -33,16 +51,15 @@ list(
       readr::read_csv("data-raw/species_names.csv")
     ),
     # List of species to pull survey data for:
-    tar_target(
+    targets::tar_target(
       survey_species,
-      westcoastdatasummary::get_species_list()
+      get_species_list()
     ),
     # File to record the assessment year and the SSC recommendations.  This file should be updated
     # by hand each cycle:
     targets::tar_target(
-      last_assess_year,
-      readr::read_csv("data-raw/assess_year_ssc_rec.csv"),
-      format = "file"
+      last_assess_year_df,
+      readr::read_csv("data-raw/assess_year_ssc_rec.csv")
     ),
     # prev_cycle used to reach into the archived folder for last cycle output
     targets::tar_target(
@@ -71,8 +88,9 @@ list(
     # downloaded from: https://reports.psmfc.org/pacfin/f?p=501:5301:2460998972960:::::
     targets::tar_target(
       harvest_spex_data,
-      readr::read_csv("data-raw/GMT015-final_specifications-2020-2025.csv"),
-      format = "file"
+      readr::read_csv(
+        "data-raw/GMT015-final_specifications-2020-2024.csv"
+      )
     ),
     # Recent GEMM data:
     targets::tar_target(
@@ -83,64 +101,52 @@ list(
     # downloaded from https://reports.psmfc.org/pacfin/f?p=501:530:2460998972960:INITIAL::::
     targets::tar_target(
       future_spex_data,
-      readr::read_csv("data-raw/GMT008-harvest_specifications_alt2-2025.csv"),
-      format = "file"
+      readr::read_csv("data-raw/GMT008-harvest specifications-2027-28.csv")
     ),
     # Revenue information downloaded from PacFIN using QueryBuilder available online:
     targets::tar_target(
       revenue_data,
-      readr::read_csv("data-raw/pacfin_revenue.csv"),
-      format = "file"
+      readr::read_csv("data-raw/pacfin_commercial_revenue_11172025.csv")
     ),
     # Tribal importance which represents subsistence and cultural significance scoring:
     targets::tar_target(
       tribal_score_data,
-      readr::read_csv("data-raw/tribal_score.csv"),
-      format = "file"
+      readr::read_csv("data-raw/tribal_score.csv")
     ),
     # Recreational importance by state:
     targets::tar_target(
       recreational_importance_scores,
-      readr::read_csv(file.path("data-raw", "recr_importance.csv")),
-      format = "file"
+      readr::read_csv("data-raw/recr_importance.csv")
     ),
     # Abundance and Assessment Frequency
     # This information is updated by summarize_stock_status() based on the most recent assessments
     targets::tar_target(
       abundance_prev_cycle,
-      readr::read_csv(here::here(
-        "data-processed",
-        prev_cycle,
-        "abundance_processed.csv"
-      )),
-      format = "file"
+      readr::read_csv("data-processed/2024/abundance_processed.csv")
     ),
     # Ecosystem top-down and bottom-up measures provided by Kristin Marshall:
     targets::tar_target(
       ecosystem_data,
-      readr::read_csv("data-raw/ecosystem_data.csv"),
-      format = "file"
+      readr::read_csv("data-raw/ecosystem_data.csv")
     ),
     # Overfished Species: Data sheet that contains information about any overfished species
     targets::tar_target(
       overfished_data,
-      readr::read_csv("data-raw/overfished_species.csv"),
-      format = "file"
+      readr::read_csv("data-raw/overfished_species.csv")
     ),
     # New research: spreadsheet that contains information about completed and in process research that
     # could be influential in a future assessment.
     # This spreadsheet needs to be updated by hand each cycle
     targets::tar_target(
       new_research,
-      readr::read_csv("data-raw/new_research.csv"),
-      format = "file"
+      readr::read_csv("data-raw/new_research.csv")
     ),
     # Pull NWFSC WCGBTS data
-    tar_target(
+    targets::tar_target(
       wcgbt_data,
-      load("data-raw/bio_pull_all_NWFSC.Combo_2025-09-12.rdata"),
+      x,
     ),
-    #tar_target(
+    #targets::tar_target(
     #  wcgbt_data,
     #  westcoastdata::pull_wcgbts(
     #    dir = here::here("data-raw"),
@@ -149,22 +155,18 @@ list(
     #  )
     #),
     # NWFSC HKL Survey Data
-    tar_target(
+    targets::tar_target(
       nwfsc_hkl_data,
-      readr::read_csv(here::here(
-        "data-raw",
-        "nwfsc_hkl_DWarehouse_version_09032025.csv"
-      ))
+      readr::read_csv("data-raw/nwfsc_hkl_DWarehouse_version_09032025.csv")
     )
   ),
 
-  # Filter Data and Clean Model Files
   list(
     # Clean model files: only done once and then can be commented out
-    tar_target(
-      clean_files,
-      clean_model_files()
-    ),
+    #tar_target(
+    #  clean_files,
+    #  clean_model_files()
+    #),
     # Apply year filters
     targets::tar_target(
       harvest_spex_filtered,
@@ -204,7 +206,7 @@ list(
     # Clean NWFSC WCGBT data
     tar_target(
       wcgbt_bio_cleaned,
-      westcoastdatasummary::clean_wcgbt_bio(
+      clean_wcgbt_bio(
         dir = here::here("data-raw"),
         species = survey_species,
         data = wcgbt_data
@@ -213,24 +215,10 @@ list(
     # Clean NWFSC HKL data
     tar_target(
       nwfsc_hkl_cleaned,
-      westcoastdatasummary::clean_nwfsc_hkl(
+      clean_nwfsc_hkl(
         dir = here::here("data-raw"),
         species = survey_species,
         data = nwfsc_hkl_data
-      )
-    )
-  ),
-
-  # Calculate Factors
-  list(
-    # Determine the new available survey data
-    tar_target(
-      new_survey_data,
-      westcoastdata::summarize_survey_new_information(
-        dir = here::here("data-processed"),
-        stock_year = last_assess_year,
-        wcgbt = wcgbt_bio_cleaned,
-        hkl = nwfsc_hkl_cleaned
       )
     ),
     # 6 Stock Status
@@ -241,12 +229,24 @@ list(
         species = species,
         years = catage_years
       )
+    )
+  ),
+
+  list(
+    # Determine the new available survey data
+    tar_target(
+      new_survey_data,
+      summarize_survey_new_information(
+        dir = here::here("data-processed"),
+        stock_year = last_assess_year_df,
+        wcgbt = wcgbt_bio_cleaned,
+        hkl = nwfsc_hkl_cleaned
+      )
     ),
     # Update abundance based on the new assessments
     targets::tar_target(
       abundance_updated,
-      readr::read_csv(here::here("data-processed", "abundance_processed.csv")),
-      format = "file"
+      readr::read_csv(here::here("data-processed", "abundance_processed.csv"))
     ),
     # 1 Fishing Mortality
     targets::tar_target(
@@ -263,7 +263,7 @@ list(
       summarize_revenue(
         revenue = commercial_revenue_filtered,
         species = species,
-        assess_year = last_assess_year
+        assess_year = last_assess_year_df
       )
     ),
     # 3 Tribal Importance
@@ -273,95 +273,7 @@ list(
         revenue = tribal_revenue_filtered,
         species = species,
         tribal_score = tribal_score_data,
-        assess_year = last_assess_year
-      )
-    ),
-    # 4 Recreational Importance
-    targets::tar_target(
-      recreational,
-      summarize_rec_importance(
-        rec_catch = rec_catch_filtered,
-        species = species,
-        rec_importance = recreational_importance_scores,
-        assess_year = last_assess_year
-      )
-    ),
-    # 5 Ecosystem
-    targets::tar_target(
-      ecosystem,
-      summarize_ecosystem(
-        ecosystem_data = ecosystem_data
-      )
-    ),
-    # 7 Assessment Frequency
-    targets::tar_target(
-      assess_frequency,
-      summarize_frequency(
-        abundance = abundance_updated,
-        ecosystem = ecosystem,
-        commercial = commercial,
-        tribal = tribal,
-        recreational = recreational,
-        assessment_year = assessment_year
-      )
-    ),
-    # 8 Constituent Demand
-    targets::tar_target(
-      constituent_demand,
-      summarize_const_demand(
-        revenue_data = revenue_data_filtered,
-        rec_importance_data = recreational,
-        fishing_mortality = fishing_mortality,
-        future_spex = future_spex_data,
-        species = species
-      )
-    ),
-    # 9 New Information
-    # targets::tar_target(
-    #  bio_params,
-    #  readr::read_csv(here::here("data-processed/species_sigmaR_catage.csv")),
-    #  format = "file"
-    # ),
-    targets::tar_target(
-      new_info,
-      summarize_new_information(
-        species = species,
-        survey_data = new_survey_data,
-        assess_year = last_assess_year,
-        new_research = new_research
-      )
-    ),
-    # 10 Rebuilding
-    targets::tar_target(
-      rebuilding,
-      summarize_rebuilding(
-        species = species,
-        overfished_data = overfished_data,
-        stock_status = stock_status,
-        assessment_year = assessment_year
-      )
-    ),
-    # Calculate the overall ranks
-    targets::tar_target(
-      rank,
-      calculate_rank(
-        fishing_mortality = fishing_mortality,
-        commercial_importance = commercial,
-        tribal_importance = tribal,
-        recreational_importance = recreational,
-        ecosystem = ecosystem,
-        stock_status = stock_status,
-        assessment_frequency = assess_frequency,
-        constituent_demand = constituent_demand,
-        new_information = new_info,
-        rebuilding = rebuilding
-      )
-    ),
-    targets::tar_target(
-      move_files,
-      move_files_to_sap(
-        dir = here::here("data-processed"),
-        move_to = "C:/Users/Chantel.Wetzel/Documents/GitHub/wcgfishSAP/tables"
+        assess_year = last_assess_year_df
       )
     )
   )
