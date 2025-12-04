7c7a8456483d451fe8d5c41c51b275da5aabc1f5
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2025-12-04 15:15:54 -0800

    add species filtering

--- R/filter_revenue.R
+++ R/filter_revenue.R
@@ -7,12 +7,49 @@
 #' @export
 #'
 filter_revenue <- function(data, type = NULL) {
+  remove <- c(
+    which(
+      data$PACFIN_SPECIES_COMMON_NAME == "BLACK ROCKFISH" &
+        data$AGENCY_CODE == "C"
+    ),
+    which(
+      data$PACFIN_SPECIES_COMMON_NAME == "BLUE ROCKFISH" &
+        data$AGENCY_CODE == "C"
+    ),
+    which(
+      data$PACFIN_SPECIES_COMMON_NAME == "DEACON ROCKFISH" &
+        data$AGENCY_CODE == "C"
+    ),
+    which(
+      data$PACFIN_SPECIES_COMMON_NAME == "CABEZON" &
+        data$AGENCY_CODE %in% c("C", "O")
+    ),
+    which(
+      data$PACFIN_SPECIES_COMMON_NAME == "CHINA ROCKFISH" &
+        data$AGENCY_CODE == "C"
+    ),
+    which(
+      data$PACFIN_SPECIES_COMMON_NAME == "COPPER ROCKFISH" &
+        data$AGENCY_CODE == "C"
+    ),
+    which(
+      data$PACFIN_SPECIES_COMMON_NAME == "KELP GREENLING" &
+        data$AGENCY_CODE == "C"
+    ),
+    which(
+      data$PACFIN_SPECIES_COMMON_NAME == "QUILLBACK ROCKFISH" &
+        data$AGENCY_CODE == "C"
+    )
+  )
+  data_filtered <- data[-remove, ]
+
   if (type == "tribal") {
-    data_filtered <- data |>
+    data_filtered <- data_filtered |>
       dplyr::filter(FLEET_CODE == "TI")
   } else {
-    data_filtered <- data |>
+    data_filtered <- data_filtered |>
       dplyr::filter(FLEET_CODE != "TI")
   }
+
   return(data_filtered)
 }
